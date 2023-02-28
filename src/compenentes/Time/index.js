ae6c75f132da6a293e021dfa2739546ed1c86009
Flávio Marques <121177356+marquesflavio@users.noreply.github.com>
2023-02-27 23:10:50 -0300

    update

--- src/compenentes/Time/index.js
+++ src/compenentes/Time/index.js
@@ -2,8 +2,8 @@ import './Time.css'
 
 const Time = (props) => {
     return(
-        <section className='time'>
-            <h3>{props.nome}</h3>  {/*props é para gerar o título de forma dinâmica*/}
+        <section className='time' style={{backgroundColor: props.corSecundaria}}>
+            <h3 style={{borderColor: props.corPrimaria}}>{props.nome}</h3>  {/*props é para gerar o título de forma dinâmica*/}
 
 
         </section>
